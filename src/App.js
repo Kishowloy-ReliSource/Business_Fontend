b3972ff47656dfaa2d6bf0a230a858917fea5d67
Kishowloy-ReliSource <126574190+Kishowloy-ReliSource@users.noreply.github.com>
2023-03-06 17:57:58 +0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,41 +6,23 @@ import { Route, Routes } from 'react-router-dom';
 import AdminPanel from './Components/adminPanel';
 
 const API_URL = "http://localhost:3000/api/v1/admins"
-
-
-
-
-
 function getAPIData()
 {
   return axios.get(API_URL).then((response)=> response.data);
 }
-
 function App() {
-
   const [ad,setAdmins]= useState([]);
-
   const fetchdata= ()=>{
     fetch(API_URL)
     .then((response)=> response.json())
     .then((data)=>{
-      //console.log(data)
       setAdmins(data)
     })
   }
-
-  useEffect(()=>
-  {
-    fetchdata()
-  }, []) 
-
   return (
     <div className="App">
-
       { <Admin admins={ad}/>  }
     </div>
-
-
   );
 }
 
