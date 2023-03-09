8a788e553159d2e19f43565991d645b1b16a332b
Kishowloy-ReliSource <kdatta@relisource.com>
2023-03-10 01:21:58 +0600

    Error Fixed in Navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,37 +1,34 @@
-import React from 'react';
-import { styled } from '@mui/material/styles';
-import { AppBar, Toolbar, IconButton, Typography, Button } from '@mui/material';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import { useNavigate,useLocation  } from 'react-router-dom';
-
+import React from "react";
+import { styled } from "@mui/material/styles";
+import { AppBar, Toolbar, IconButton, Typography, Button } from "@mui/material";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const StyledAppBar = styled(AppBar)({
-  backgroundColor: '#2196f3',
+  backgroundColor: "#2196f3",
 });
 
 const StyledIconButton = styled(IconButton)({
-  marginRight: '16px',
+  marginRight: "16px",
 });
-
 
 function Navbar(props) {
   const { title } = props;
-  const navigate=useNavigate();
-  const location =useLocation();
-  
-    console.log(location.pathname);
-  const handleBackClick = () => {
-      navigate(-1);
-  };
-  const logoutButton= () =>{
-    navigate('/');
-  }
+  const navigate = useNavigate();
+  const location = useLocation();
 
+  console.log(location.pathname);
+  const handleBackClick = () => {
+    navigate(-1);
+  };
+  const logoutButton = () => {
+    navigate("/");
+  };
 
   return (
     <StyledAppBar position="static">
       <Toolbar>
-      {location.pathname!='/' &&
+        {location.pathname != "/" && (
           <StyledIconButton
             edge="start"
             color="inherit"
@@ -40,18 +37,17 @@ function Navbar(props) {
           >
             <ArrowBackIcon />
           </StyledIconButton>
-        }
-        
+        )}
+
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           {title}
         </Typography>
-        {location.pathname!='/' &&
-        <Button variant='outlined' color='error' onClick={logoutButton}>
+        {location.pathname != "/" && (
+          <Button variant="outlined" color="error" onClick={logoutButton}>
             Log Out
           </Button>
-}
+        )}
       </Toolbar>
-      
     </StyledAppBar>
   );
 }
